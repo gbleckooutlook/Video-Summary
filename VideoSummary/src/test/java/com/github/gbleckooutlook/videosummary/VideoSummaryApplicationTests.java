e71d23d4773fb20dbfaf16e9e299aba2ca65e486
Leandro <leandro.desenvolvedor@outlook.com>
2024-11-03 17:28:02 -0300

    Add initial Spring Boot project setup
    Introduced the initial structure of a Spring Boot project including a basic Maven configuration, application properties, and a simple Spring Boot application entry point. Added .gitignore and .gitattributes files for handling specific project file exclusions and line endings.

---
+++ VideoSummary/src/test/java/com/github/gbleckooutlook/videosummary/VideoSummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gbleckooutlook.videosummary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSummaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
